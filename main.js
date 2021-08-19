0b0c8d4161ec2bc190cbd7d2867787ace7fbeba7
Omg-A <67595078+Omg-A@users.noreply.github.com>
2021-08-19 12:55:54 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,28 @@ function preload(){
 function setup(){
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet has been Initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("Results of nose X : " + results[0].pose.nose.x);
+        console.log("Results of nose Y : " + results[0].pose.nose.y);
+    }
 }
 
 function draw(){
-
+    image(video, 0, 0, 300, 300);
 }
 
 function take_snapshot(){
