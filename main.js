ac8f734117e315e80165cb444bb02f0807c5b0c0
Omg-A <67595078+Omg-A@users.noreply.github.com>
2021-08-21 12:41:37 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-function preload(){
+noseX = 0;
+noseY = 0;
 
+
+function preload(){
+    clownNose = loadImage("https://i.postimg.cc/Bbx7FCC5/clown-nose.png"); 
 }
 
 function setup(){
@@ -20,13 +24,19 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        console.log("Results of nose X : " + results[0].pose.nose.x);
-        console.log("Results of nose Y : " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 15;
+        noseY = results[0].pose.nose.y - 15;
+        console.log("Results of nose X : " + noseX);
+        console.log("Results of nose Y : " + noseY);
     }
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    //fill(255, 0, 0);
+    //stroke(255, 0, 0);
+    //circle(noseX, noseY, 20);
+    image(clownNose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot(){
